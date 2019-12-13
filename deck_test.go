3fbab34b6555f6e42c0dcb0e3a34d09dd888e770
John <john.19col@gmail.com>
2019-12-13 06:56:34 -0500

    Test how many cards there are per suit in the deck

--- deck_test.go
+++ deck_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"strings"
+)
 
-func TestNewDeck(t *testing.T) {
+func TestNewDeck_HasTheExpectedSize(t *testing.T) {
 	deck := NewDeck()
 
 	expectedDeckSize := 16
@@ -10,4 +13,28 @@ func TestNewDeck(t *testing.T) {
 	if actualDeckSize != expectedDeckSize {
 		t.Errorf("Expected deck length is %v but got %v", expectedDeckSize, actualDeckSize)
 	}
+}
+
+func TestNewDeck_Has4CardsForEachSuit(t *testing.T) {
+	suits := []string{"Hearts","Spades","Dimonds","Clubs"}
+	deck := NewDeck()
+	expectedCardsPerSuit := 4
+
+	for _, suit := range suits {
+		suitCards := filterDeckBySuit(deck, suit)
+		actualCardsPerSuit := len(suitCards)
+		if actualCardsPerSuit != expectedCardsPerSuit {
+			t.Errorf("Expected cards for suit %v was %v, but got %v", suit, expectedCardsPerSuit, actualCardsPerSuit)
+		}
+	}
+}
+
+func filterDeckBySuit(deck Deck, suit string) []string {
+	suitCards := []string{}
+	for _, card := range deck {
+		if strings.HasSuffix(card, suit) {
+			suitCards = append(suitCards, card)
+		}
+	}
+	return suitCards
 }
