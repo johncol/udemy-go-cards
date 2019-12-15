c85d76797d95a98f68a3ee0a50ed76837ccd175f
John <john.19col@gmail.com>
2019-12-14 22:54:59 -0500

    Add test to save/load deck to/from file

--- deck_test.go
+++ deck_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 	"strings"
+	"os"
 )
 
 func TestNewDeck_HasTheExpectedSize(t *testing.T) {
@@ -27,6 +28,25 @@ func TestNewDeck_Has4CardsForEachSuit(t *testing.T) {
 			t.Errorf("Expected cards for suit %v was %v, but got %v", suit, expectedCardsPerSuit, actualCardsPerSuit)
 		}
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	testFileName := "test-deck.txt"
+	_ = os.Remove(testFileName)
+
+	deck := NewDeck()
+	err := deck.SaveToFile(testFileName)
+	if err != nil {
+		t.Fatal("SaveToFile returned error: ", err)
+	}
+
+	deckFromFile := NewDeckFromFile(testFileName)
+
+	if len(deck) != len(deckFromFile) {
+		t.Errorf("Original deck and deck from file are not the same")
+	}
+	
+	_ = os.Remove(testFileName)
 }
 
 func filterDeckBySuit(deck Deck, suit string) []string {
